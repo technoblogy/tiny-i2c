3319c1c58629fdc28aeea4364b126dd730e1527c
David Johnson-Davies <david@interface.co.uk>
2019-05-28 22:28:40 +0100

    Update TinyI2CMaster.h

--- TinyI2CMaster.h
+++ TinyI2CMaster.h
@@ -1,6 +1,6 @@
 /* Tiny I2C
 
-   David Johnson-Davies - www.technoblogy.com - 14th April 2018
+   David Johnson-Davies - www.technoblogy.com - 28th May 2019
    
    CC BY 4.0
    Licensed under a Creative Commons Attribution 4.0 International license: 
